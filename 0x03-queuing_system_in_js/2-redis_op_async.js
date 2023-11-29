6ab872904971e2ac7293c1806d087ce1fa8ce804
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2023-11-29 07:46:24 +0100

    initial commit

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,32 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+const client = redis.createClient();
+
+  client.on('connect', () => {
+    console.log('Redis client connected to the server');
+  });
+  
+  client.on('error', (err) => {
+    console.log(`Redis client not connected to the server: ${err.message}`);
+  });
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+const get = promisify(client.get).bind(client);
+
+async function displaySchoolValue(schoolName) {
+  const result = await get(schoolName).catch((err) => {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+    console.log(res);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
